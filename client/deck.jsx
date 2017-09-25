02ad8a4155a6436c32a722e3ea9e586fc50779e0
Katie <krblacketor@gmail.com>
2017-09-25 17:57:32 -0400

    start pseudocode for deck of cards

---
+++ client/deck.jsx
@@ -0,0 +1,12 @@
+//start with Stack data-structure
+  //empty array of cards
+//add methods to Stack.prototype
+  //make a deck
+    //array of card numbers, & faces - ranks (13)
+    //array of suits (4)
+    //create cards array (ranks * suits) 
+  //shuffle a deck
+    //create a temporary storage
+    //use Math.random to reorder the deck
+  //deal cards
+  //draw cards
