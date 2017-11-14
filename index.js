40659d58473573f4eb52fc6a8392d875f9fc2dc2
Kaitlin Blacketor <kaitlinblacketor@Kaitlins-MacBook-Pro.local>
2017-11-13 19:53:11 -0500

    fix card backs, work on sockets'

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const path = require('path');
 const socket = require('socket.io');
 const db = require('./db/connect.js');
 const routes = require('./routes.js');
-
+const PeerServer = require('peer').PeerServer;
 const app = express();
 const compiler = webpack(webpackConfig);
 
@@ -27,23 +27,35 @@ app.set('port', (process.env.PORT || 3000));
 //   historyApiFallback: true,
 // }));
 
-const server = app.listen(app.get('port'), () => {
+const expressServer = app.listen(app.get('port'), () => {
   console.log(`Listening on ${app.get('port')}`);
 });
 
 // socket setup
-const io = socket(server);
+const io = socket(expressServer);
+const peerServer = new PeerServer({ port: 9000, path: '/play' });
 
-io.on('connection', (socket) => {
-
-  console.log('made socket connection', socket.id);
-  socket.on('disconnect', () => console.log('Client disconnected'));
-  // Handle chat event
-  socket.on('chat', function(data){
-    // console.log(data);
-    io.sockets.emit('chat', data);
-  });
-  socket.on('typing', (data) => {
-    socket.broadcast.emit('typing', data)
-  })
+peerServer.on('connection', (id) => {
+  io.emit(Topics.USER_CONNECTED, id);
+  console.log(`User connected with # ${id}`);
 });
+
+peerServer.on('disconnect', (id) => {
+  io.emit(Topics.USER_DISCONNECTED, id);
+  console.log(`User #${id} disconnected.`);
+})
+
+
+// io.on('connection', (socket) => {
+
+//   console.log('made socket connection', socket.id);
+//   socket.on('disconnect', () => console.log('Client disconnected'));
+//   // Handle chat event
+//   socket.on('chat', function(data){
+//     // console.log(data);
+//     io.sockets.emit('chat', data);
+//   });
+//   socket.on('typing', (data) => {
+//     socket.broadcast.emit('typing', data)
+//   })
+// });
